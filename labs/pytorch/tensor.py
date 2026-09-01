167d48297765ee505df29b4a17a5b5af971675e8
niepeixuan.k <niepeixuan.k@bytedance.com>
2026-09-01 12:59:48 +0000

    feat: basic use for numpy

---
+++ labs/pytorch/tensor.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Reference: https://numpy.org/doc/stable/user/absolute_beginners.html
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+print(a[0])
+print(a[1:3])
+print(a.dtype)
+print(a.shape)
+
+b = np.array([[1, 2, 3], [4, 5, 6]])
+print(b)
+print(b[0, 0])
+print(b.dtype)
+print(b.shape)
