d739545235e27b5d01f09f2709366ab73b791845
niepeixuan.k <niepeixuan.k@bytedance.com>
2026-09-01 13:51:14 +0000

    feat: basic use for numpy

--- labs/pytorch/tensor.py
+++ labs/pytorch/tensor.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 # Reference: https://numpy.org/doc/stable/user/absolute_beginners.html
@@ -10,7 +9,7 @@ print(a.dtype)
 print("a.size:", a.size)
 print("a.ndim:", a.ndim)
 print("a.shape:", a.shape)
-print("Length of shape:", len(a.shape))
+print(a.size == np.prod(a.shape))
 
 b = np.array([[1, 2, 3], [4, 5, 6]])
 print(b)
@@ -19,3 +18,4 @@ print(b.dtype)
 print("b.size:", b.size)
 print("b.ndim:", b.ndim)
 print("b.shape:", b.shape)
+print(b.size == np.prod(b.shape))
